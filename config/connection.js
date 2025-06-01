86d7879622a9190a532783a89b2de603e6763299
virendrakushwah123 <120722760+virendrakushwah123@users.noreply.github.com>
2025-06-01 17:38:55 +0530

    db update

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
 /**  including .env file */
-var mongoUrl = process.env.MONGO_URL;
+//var mongoUrl = process.env.MONGO_URL;
+
+var mongoUrl = "mongodb+srv://rksonline01:otUmyxInZ6NgUGYH@carbooking.sx7nd7b.mongodb.net/";
+
+
 
 var MongoClient = require('mongodb').MongoClient;
 
 var _db;
 
-var dbName = process.env.DATABASE;
+//var dbName = process.env.DATABASE;
+dbName = "carbooking";
 
 const client = new MongoClient(mongoUrl);
 
